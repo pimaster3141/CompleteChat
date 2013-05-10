d2868f1b2263e103915cc077f39c706a035b81f0
Jason Yang <jasony@mit.edu>
2013-05-10 11:27:46 -0400

    changed package tree

---
+++ src/server/lists/ChatUserList.java
@@ -0,0 +1,20 @@
+package server.lists;
+
+public class ChatUserList extends UserList
+{
+
+	private final String name;
+
+	public ChatUserList(String name)
+	{
+		super();
+		this.name = name;
+	}
+
+	protected String getList()
+	{
+		String list = super.getList();
+		return "chatUserList " + this.name + " " + list;
+	}
+
+}
---
+++ src/server/lists/ServerUserList.java
@@ -0,0 +1,11 @@
+package server.lists;
+
+public class ServerUserList extends UserList
+{
+
+	protected String getList()
+	{
+		String list = super.getList();
+		return "serverUserList " + list;
+	}
+}
