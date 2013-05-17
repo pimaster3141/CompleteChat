d267f24ef8c95560dfe321fa97d2aeda4c082b83
Jenny <jhl989@mit.edu>
2013-05-17 15:52:01 -0400

    Moved the parsing from MainWindow into Client. It's much easier to read now~

--- src/client/gui/MainWindow.java
+++ src/client/gui/MainWindow.java
@@ -11,7 +11,7 @@ import javax.swing.text.BadLocationException;
 
 import client.*;
 
-public class MainWindow extends JFrame implements ActionListener{
+public class MainWindow extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private final JTabbedPane tabs;
     private final JMenuBar menuBar;
@@ -57,7 +57,7 @@ public class MainWindow extends JFrame implements ActionListener{
         logout.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 client.send("disconnect " + client.getUsername());
-                MainWindow.this.dispose();
+                quit();
             }
         });
     }
@@ -139,9 +139,6 @@ public class MainWindow extends JFrame implements ActionListener{
         mainTab.setClient(c);
     }
     
-    public void setLoginWindow(LoginWindow l) {
-    }
-    
     public void addRooms(Object[] ChatRooms) {
         mainTab.addRooms(ChatRooms);
     }
@@ -162,156 +159,130 @@ public class MainWindow extends JFrame implements ActionListener{
         return null;
     }
     
-    /**
-     * This method parses the input taken in from the Client and
-     * creates SwingUtilities to invokeLater and set them onto the
-     * event dispatch thread.  The respective action is performed
-     * based on the input from the server.
-     */
-    public void actionPerformed(ActionEvent e) {
-        String input = e.getActionCommand();
-        System.out.println(input);
-        
-        int firstSpaceIndex = input.indexOf(' ');
-        String command;
-        if (firstSpaceIndex ==  -1) {
-        	command = input;
-        	firstSpaceIndex = input.length()-1;
-        }
-        else
-        	command = input.substring(0, firstSpaceIndex);
-        
-        if(command.equals("disconnectedServerSent")) {
-            System.out.println("Actually we'll never get here because my logout was too beautiful " +
-            		"for this world.");
-        } else if(command.equals("message")) {
-            int secondSpaceIndex = input.indexOf(' ', firstSpaceIndex+1);
-            int thirdSpaceIndex = input.indexOf(' ', secondSpaceIndex+1);
-            final String chatRoomName = input.substring(firstSpaceIndex + 1, secondSpaceIndex);
-            final String userName = input.substring(secondSpaceIndex + 1, thirdSpaceIndex);
-            final String message = input.substring(thirdSpaceIndex + 1);
-            SwingUtilities.invokeLater(new Runnable() {
-                public void run() {
-                    if(connectedRoomsCurrent.containsKey(chatRoomName)){
-                        ChatRoomClient roomCurrent = connectedRoomsCurrent.get(chatRoomName);
-                        try {
-                            roomCurrent.addMessage(new Message(userName, message));
-                        } catch (BadLocationException e1) {
-                            e1.printStackTrace();
-                        }
-                    }
-                }
-            });
-        } else {
-            final String[] list = input.substring(firstSpaceIndex+1).split(" ");
-            if(command.equals("serverUserList")) {
-                SwingUtilities.invokeLater(new Runnable() {
-                    public void run() {
-                        allUsers.clear();
-                        for(int i = 0; i < list.length; i++) {
-                            allUsers.addElement(list[i]);
-                        }
-                        mainTab.setListModels(allUsers, allRooms);
-                    }
-                });
-            } else if(command.equals("serverRoomList")) {
-                SwingUtilities.invokeLater(new Runnable() {
-                    public void run() {
-                        System.out.println("Updating serverRoomList");
-                        allRooms.clear();
-                        for(int i = 0; i < list.length; i++) {
-                            System.out.println("Adding room: " + list[i]);
-                            allRooms.addElement(list[i]);
-                        }
-                        mainTab.setListModels(allUsers, allRooms);
-                    }
-                });
-
-            } else if(command.equals("chatUserList")) {
-                SwingUtilities.invokeLater(new Runnable() {
-                    public void run() {
-                        String chatName = list[0];
-                        if (connectedRoomsCurrent.containsKey(chatName)) {
-                            ArrayList<String> newChatList = new ArrayList<String>();
-                            for (int i = 1; i < list.length; i++) {
-                                System.err.println("adding: " + list[i]);
-                                newChatList.add(list[i]);
-                            }
-                            ChatRoomClient roomCurrent = connectedRoomsCurrent.get(list[0]);
-                            roomCurrent.updateUsers(newChatList);
-                        }
-                    }
-                });
-
-            } else if(command.equals("clientRoomList")) {
-                SwingUtilities.invokeLater(new Runnable() {
-                    public void run() {
-                        String user = list[0];
-                        for(int i = 1; i < list.length; i++) {
-                            if(!connectedRoomsCurrent.containsKey(list[i])) {
-                                client.send("disconnect " + user);
-                            }
-                        }
-                    }
-                });
-                
-
-            } else if(command.equals("connectedRoom")) {
-                SwingUtilities.invokeLater(new Runnable() {
-                    public void run() {
-                        String roomName = list[0];
-                        if(connectedRoomsHistory.containsKey(roomName)) {
-                            if(connectedRoomsCurrent.containsKey(roomName)) {
-                            } else {
-                                ChatRoomClient chat = connectedRoomsHistory.get(roomName);
-                                connectedRoomsCurrent.put(roomName, chat);
-                                addCloseableTab(roomName, new ChatTab(roomName, client, MainWindow.this));
-                            }
-                        } else {
-                            
-                                ChatRoomClient chat = new ChatRoomClient(roomName, client.getUsername());
-                                connectedRoomsCurrent.put(roomName, chat);
-                                connectedRoomsHistory.put(roomName, chat);
-                                addCloseableTab(roomName, new ChatTab(roomName, client, MainWindow.this));
-                        }
-                    }
-                });
-                
-
-            } else if(command.equals("disconnectedRoom")) {
-                SwingUtilities.invokeLater(new Runnable() {
-                    public void run() {
-                        String roomName = list[0];
-                        if (connectedRoomsCurrent.containsKey(roomName)) {
-                            JPanel removedRoom = findTab(roomName);
-                            if (removedRoom != null) {
-                                int tabIndex = tabs.indexOfComponent(removedRoom);
-                                tabs.remove(tabIndex);
-                            }
-                        } 
-                    }
-                });
-                
-
-            } else if (command.equals("invalidRoom")) {
-                final StringBuilder errorMessage = new StringBuilder();
-                for (int i = 1; i < list.length; i++) {
-                    errorMessage.append(list[i] + " ");
-                }
-                SwingUtilities.invokeLater(new Runnable() {
-                    public void run() {
-                        MainWindow.this.displayErrorMessage(errorMessage.toString());
-                    }
-                });
-            } else {
-                System.err.println("Derp we seem to have ended up in dead code");
-            }
-        }
-    }
 
     
-    private void displayErrorMessage(String errorMessage) {
+    public void updateConversation(String chatRoomName, String userName, String message) {
+        final String c = chatRoomName;
+        final String u = userName;
+        final String m = message;
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                if(connectedRoomsCurrent.containsKey(c)){
+                    ChatRoomClient roomCurrent = connectedRoomsCurrent.get(c);
+                    try {
+                        roomCurrent.addMessage(new Message(u, m));
+                    } catch (BadLocationException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+    
+    public void displayErrorMessage(String errorMessage) {
         JOptionPane.showMessageDialog(this, errorMessage.toString(), "Error", JOptionPane.WARNING_MESSAGE);
+    }
+    
+    public void updateMainUserList(String[] users) {
+        final String[] list = users;
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                allUsers.clear();
+                for(int i = 0; i < list.length; i++) {
+                    allUsers.addElement(list[i]);
+                }
+                mainTab.setListModels(allUsers, allRooms);
+            }
+        });
+    }
+    
+    public void updateMainChatList(String[] chats) {
+        final String[] list = chats;
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                System.out.println("Updating serverRoomList");
+                allRooms.clear();
+                for(int i = 0; i < list.length; i++) {
+                    System.out.println("Adding room: " + list[i]);
+                    allRooms.addElement(list[i]);
+                }
+                mainTab.setListModels(allUsers, allRooms);
+            }
+        });
+    }
+    
+    public void updateChatUserList(String chatname, ArrayList<String> users) {
+        final String c = chatname;
+        final ArrayList<String> list = users;
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                if (connectedRoomsCurrent.containsKey(c)) {
+                    ChatRoomClient roomCurrent = connectedRoomsCurrent.get(c);
+                    roomCurrent.updateUsers(list);
+                }
+            }
+        });
+    }
+    
+    public void updateUserChatList(String username, String[] chats) {
+        final String u = username;
+        final String[] list = chats;
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                if (u.equals(client.getUsername())) {
+                    for(int i = 1; i < list.length; i++) {
+                        if(!connectedRoomsCurrent.containsKey(list[i])) {
+                            client.send("disconnect " + u);
+                        }
+                    }
+                }
+            }
+        });
+    }
+    
+    public void joinChat(String chatname) {
+        final String c = chatname;
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                if(connectedRoomsHistory.containsKey(c)) {
+                    if(connectedRoomsCurrent.containsKey(c)) {
+                    } else {
+                        ChatRoomClient chat = connectedRoomsHistory.get(c);
+                        connectedRoomsCurrent.put(c, chat);
+                        addCloseableTab(c, new ChatTab(c, MainWindow.this));
+                    }
+                } else {
+                    
+                        ChatRoomClient chat = new ChatRoomClient(c, client.getUsername());
+                        connectedRoomsCurrent.put(c, chat);
+                        connectedRoomsHistory.put(c, chat);
+                        addCloseableTab(c, new ChatTab(c, MainWindow.this));
+                }
+            }
+        });
+    }
+    
+    public void leaveChat(String chatname) {
+        final String c = chatname;
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                if (connectedRoomsCurrent.containsKey(c)) {
+                    JPanel removedRoom = findTab(c);
+                    if (removedRoom != null) {
+                        int tabIndex = tabs.indexOfComponent(removedRoom);
+                        tabs.remove(tabIndex);
+                    }
+                } 
+            }
+        });
+    }
+    
+    public void quit() {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                dispose();
+            }
+        });
     }
     
     /**
